dd94403322abf94217bb51e64dd1b92c8f431c0d
Georg Kromer <georgkromer@protonmail.com>
2020-05-11 20:39:01 +0200

    Added project lombok

--- Backend/src/main/java/de/byebyeco2/Entities/RoofOffer.java
+++ Backend/src/main/java/de/byebyeco2/Entities/RoofOffer.java
@@ -1,12 +1,15 @@
 package de.byebyeco2.Entities;
 
+import lombok.*;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name="roof_offers")
+@Getter @Setter @NoArgsConstructor @ToString
 public class RoofOffer extends Listing {
 
-    @Id
+    @Id @Setter(AccessLevel.PROTECTED)
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
@@ -18,72 +21,9 @@ public class RoofOffer extends Listing {
     private int children;
     private int adults;
 
-    public RoofOffer() {
-    }
-
     public RoofOffer(int orientation, int roofTilt, int roofType) {
         this.orientation = orientation;
         this.roofTilt = roofTilt;
         this.roofType = roofType;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public int getOrientation() {
-        return orientation;
-    }
-
-    public void setOrientation(int orientation) {
-        this.orientation = orientation;
-    }
-
-    public int getRoofTilt() {
-        return roofTilt;
-    }
-
-    public void setRoofTilt(int roofTilt) {
-        this.roofTilt = roofTilt;
-    }
-
-    public int getRoofType() {
-        return roofType;
-    }
-
-    public void setRoofType(int roofType) {
-        this.roofType = roofType;
-    }
-
-    public boolean isOwnUse() {
-        return ownUse;
-    }
-
-    public void setOwnUse(boolean ownUse) {
-        this.ownUse = ownUse;
-    }
-
-    public int getSizeHousehold() {
-        return sizeHousehold;
-    }
-
-    public void setSizeHousehold(int sizeHousehold) {
-        this.sizeHousehold = sizeHousehold;
-    }
-
-    public int getChildren() {
-        return children;
-    }
-
-    public void setChildren(int children) {
-        this.children = children;
-    }
-
-    public int getAdults() {
-        return adults;
-    }
-
-    public void setAdults(int adults) {
-        this.adults = adults;
     }
 }
